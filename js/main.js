23ce695952ce246cee4d3cf2175a69f126690e03
Nam Wook Kim <namwkim85@gmail.com>
2018-02-18 19:21:51 -0500

    materialize test

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// initialize side-navigation bar
+
+var elem = document.querySelector('.sidenav');
+console.log(elem);
+var instance = M.Sidenav.init(elem);
+
+var elem = document.querySelector('.carousel');
+var instance = M.Carousel.init(elem);
